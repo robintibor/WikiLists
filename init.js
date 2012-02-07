fb9c118c8e44be50eee2a2b47c621332314db46a
robintibor <robintibor@gmail.com>
2012-02-07 18:34:32 +0000

    first working version of dummy parser :)

--- init.js
+++ init.js
@@ -2,7 +2,7 @@
 alert('test1');
 
 var wikiLists = new function() {
-    this.USER = 'ikset';
+    this.USER = 'robintibor';
     var addJavaScriptFiles = function(fileURLS) {
         for (var i = 0; i < fileURLS.length; i++) {
             var javaScriptSource = fileURLS[i];
@@ -16,7 +16,8 @@ var wikiLists = new function() {
     this.init = function() {
         var javaScriptSources = [ 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
                                   'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js',
-                                  'http://c9.io/' + wikiLists.USER + '/wikilists/workspace/UIMenu.js'];
+                                  'http://c9.io/' + wikiLists.USER + '/wikilists/workspace/UIMenu.js',
+                                  'http://c9.io/' + wikiLists.USER + '/wikilists/workspace/parseListElements.js'];
         addJavaScriptFiles(javaScriptSources);
     };
 };
