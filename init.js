39b930c9ba511383c12fc7b680096edee863aa65
robintibor <robintibor@gmail.com>
2012-02-07 13:53:08 +0000

    created namespace

--- init.js
+++ init.js
@@ -1,16 +1,22 @@
 // Load Javascript files
 alert('test1');
-function addJavaScriptFiles(fileURLS) {
-    for (var i = 0; i < fileURLS.length; i++) {
-        var javaScriptSource = fileURLS[i];
-        var headID = document.getElementsByTagName("head")[0];         
-        var newScript = document.createElement('script');
-        newScript.type = 'text/javascript';
-        newScript.src = javaScriptSource;
-        headID.appendChild(newScript);
-    }    
-}
-var javaScriptSources = [ 'http://c9.io/robintibor/wikilists/workspace/alertTest.js' , 
-                          'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' ];
-addJavaScriptFiles(javaScriptSources);
+var wikiLists = new function() {
+    var addJavaScriptFiles = function(fileURLS) {
+        for (var i = 0; i < fileURLS.length; i++) {
+            var javaScriptSource = fileURLS[i];
+            var headID = document.getElementsByTagName("head")[0];         
+            var newScript = document.createElement('script');
+            newScript.type = 'text/javascript';
+            newScript.src = javaScriptSource;
+            headID.appendChild(newScript);
+        }
+    };
+    this.init = function() {
+        var javaScriptSources = [ 'http://c9.io/robintibor/wikilists/workspace/alertTest.js' , 
+                                  'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' ];
+        addJavaScriptFiles(javaScriptSources);
+    };
+};
+wikiLists.init();
+
 // Start Main Parsing Routine
