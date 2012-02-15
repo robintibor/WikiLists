89f2424e951c3050a7f106457f82ea5c98c881a0
robintibor <robintibor@gmail.com>
2012-02-15 20:27:43 +0000

    'some tests done for parsing'

--- tests/parseListElementsTest.js
+++ tests/parseListElementsTest.js
@@ -3,14 +3,20 @@ wl.parserTests = new function() {
         for (var i = 0; i < testSites.length; i++) {
             var testSite = testSites[i].site;
             var testSelector = testSites[i].selector;
-            parseOneSite(testSite, testSelector);
+            var filters = testSites[i].filters;
+            parseOneSite(testSite, testSelector, filters);
         }
     };
-    var parseOneSite = function(testSite, testSelector) {
+    var parseOneSite = function(testSite, testSelector, filters) {
         $.get(testSite, function(htmlString) {
-            var expectedListElements = $(htmlString).find(testSelector);
-            var parserListElements = wl.parser.parseListElements(htmlString);
-            equal(expectedListElements.size(), parserListElements.size(),
+            var htmlDOM = $(htmlString);
+            var expectedListElements = htmlDOM.find(testSelector);
+            for (var i = 0; i < filters.length; i++) {
+                var filter = filters[i];
+                expectedListElements = expectedListElements.not(filter);
+            }
+            var parserListElements = wl.parser.parseListElements(htmlDOM);
+            equal(parserListElements.size(), expectedListElements.size(),
                     'parser should get as many elements as expected');
             for (var i = 0; i < parserListElements.size(); i++) {
                 equal($(parserListElements[i]).html(), $(expectedListElements[i]).html());
@@ -19,7 +25,8 @@ wl.parserTests = new function() {
         
     };
     var testSites = [ { site: 'test-sites/List_of_healthcare_reform_advocacy_groups_in_the_United_States.html',
-                        selector: '.mw-content-ltr > ul li a[href!="http://www.uhcan.org/"]' }];
+                        selector: '.mw-content-ltr > ul li a[href!="http://www.uhcan.org/"]',
+                        filters: ['#toc a', 'h2:has(span#See_also) ~ * * a']}];
 };
         
 
@@ -30,22 +37,21 @@ test('testParsingListElements', function() {
     setTimeout(function() {  
         start();  
     }, timeToLoadSites);  
-})
+});
 
-test('firstTests()', function() {
-    ok(wl.parser.dummyTest(), 'should be true');
-})
+test('createSelectors', function() {
+    var htmlElement = $('<a href = "wiki" id="testid">bla</a>')[0];    
+    equal(wl.parser.createTagIdAndTextSelector(htmlElement), 
+        'A#testid:contains("bla")', 'selector with id tag text should be correct');    
+    equal(wl.parser.createTagIdAndTextSelector($('<a></a>')[0]), 
+        'A', 'selector without anything should be correct');    
+});
 
-
-asyncTest('asynchronous test', function() {
-    // The test is automatically paused
-
-	setTimeout(function() {
-		ok(true);
-
-		// After the assertion has been called,
-		// continue the test
-		start();
-	}, 100)
-})
+// Test for me for understanding jquery
+test('add elements to jquery set', function () {
+    var emptyJQueryList = $();
+    equal(emptyJQueryList.length, 0);
+    emptyJQueryList = emptyJQueryList.add('<h1><a>test</a></h1>');
+    equal(emptyJQueryList.length, 1);
+});
 
