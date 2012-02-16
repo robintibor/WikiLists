b5a21a7da3f30b7148cf3c88372d9029210d0f3a
robintibor <robintibor@gmail.com>
2012-02-16 11:22:19 +0000

    'more tests, alert test deleted, some directory structure'

---
+++ parser/element-path.js
@@ -0,0 +1,21 @@
+var wikiLists = wikiLists || {};
+var wl = wikiLists;
+wl.parser = wl.parser || {};
+wl.parser.elementPath = function(element) {
+    var element = element;
+    this.element = function() {
+        return element;
+    }
+    var reverseNodeList = [];
+    this.numNodes = function() {
+        return reverseNodeList.length;
+    }
+    this.addNodeToFront = function(tag, isChildNr) {
+        var newNode = new node(tag, isChildNr);
+        reverseNodeList.push(newNode);
+    };
+    var node = function(tag, isChildNr) {
+        this.tag = tag;
+        this.isChildNr = isChildNr;
+    }
+}
---
+++ tests/element-path-test.js
@@ -0,0 +1,21 @@
+
+test('assigning and retrieving elements', function() {
+    var element = $('<a> test</a>');
+    var elementPath = new wl.parser.elementPath(element);
+    equal(elementPath.element(), element, 'element of path should be retrievable');
+    deepEqual(elementPath.element(), element, 'element of path should be deeply equal');
+    var newElement = $('<b>test</b>');
+    var newElementPath = new wl.parser.elementPath(newElement);
+    equal(newElementPath.element(), newElement, 'element of new path should be retrievable');
+    deepEqual(newElementPath.element(), newElement, 'element of new path should be deeply equal');
+    notEqual(newElementPath.element, elementPath.element, 'different elements shouldn\'t be equal');
+})
+
+test('adding nodes to the path, checking num of nodes', function() {
+    var elementPath = new wl.parser.elementPath();
+    equal(elementPath.numNodes(), 0, "should be empty at start");
+    elementPath.addNodeToFront('a', 2);
+    equal(elementPath.numNodes(), 1, "should have one node now");
+    elementPath.addNodeToFront('b', 3);
+    equal(elementPath.numNodes(), 2, "should have two nodes now");
+})
