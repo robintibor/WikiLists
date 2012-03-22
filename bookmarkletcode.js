6779c75c9d50c1e97bc03e971c5ad2223ccb609d
robintibor <robintibor@gmail.com>
2012-03-22 18:09:19 +0000

    added frontend adress, removed user, for allowing github to be used as code base as well

--- bookmarkletcode.js
+++ bookmarkletcode.js
@@ -1,1 +1,3 @@
 javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://c9.io/robintibor/wikilists/workspace/init.js';})();
+// for github:
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.github.com/robintibor/WikiLists/master/init.js';})();
