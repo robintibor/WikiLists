cfedd5db9f9c545ca8e24a95596a27b71e116dbe
robintibor <robintibor@gmail.com>
2012-03-23 15:19:17 +0000

    now functionality to get hitgroup added

--- logic/client.js
+++ logic/client.js
@@ -1,6 +1,10 @@
 var wikiLists = wikiLists || {};
 var wl = wikiLists;
+// TODO(Robin): split up client into broccoli client and other client
+// split broccoli client below into iframe client and broccoli client
+// i.e. cleint broccoliclient -> wikilistclient broccoliclient iframelistener
 wl.client = new function() {
+
     this.getListElementsAndQueryStringForWikiURL = function(wikiURL,
         callbackForResponse) {
         var stromboliRequestURL = createStromboliRequestURL(wikiURL);
@@ -15,13 +19,31 @@ wl.client = new function() {
         });
     };
 
-    this.getBroccoliInstances = function (queryString, broccoliHost,
-    broccoliPort, numberOfInstances, callback) {
+    this.getBroccoliHitGroup = function(hitGroupIndex, queryString,
+            broccoliHost, broccoliPort, callback) {
         wl.client.getURL(
-            'http://' + broccoliHost + '.informatik.uni-freiburg.de:' + 
-             broccoliPort + '/?s=' + queryString + 
+            createBroccoliQueryURL(queryString, broccoliHost, broccoliPort) +
+            '&nofinstances=0&firsthitgroup=' + hitGroupIndex + '&nofhitgroups=1',
+            callback);
+    };
+
+    this.getBroccoliInstances = function (queryString, broccoliHost,
+            broccoliPort, numberOfInstances, callback) {
+        wl.client.getURL(
+            createBroccoliQueryURL(queryString, broccoliHost, broccoliPort) +
             '&nofinstances=' + numberOfInstances + '&nofhitgroups=0',
              callback);
+    };
+
+    var createBroccoliQueryURL = function(queryString, 
+        broccoliHost, broccoliPort) {
+        return createBroccoliURL(broccoliHost, broccoliPort) + '/?s=' + 
+            queryString;
+    };
+
+    var createBroccoliURL = function(broccoliHost, broccoliPort) {
+        return  'http://' + broccoliHost + '.informatik.uni-freiburg.de:' + 
+             broccoliPort;
     };
 
     this.getURL = function(urlString, callback) {
@@ -48,25 +70,51 @@ wl.client = new function() {
 // This "client" waits for messages from the broccoli 
 // iframe and acts upon them :)
 wl.broccoliClient = new function() {
+    this.lastQueryString = '';
+    this.broccoliHost = '';
+    this.broccoliPort = '';
+
     this.listenForQueryStringFromBroccoliFrame = function() {              
         window.addEventListener('message',function(event) {                  
                         var broccoliJSON = $.parseJSON(event.data);
                         receiveBroccoliJSON(broccoliJSON);
         }, false);
     };
+
     var receiveBroccoliJSON = function (broccoliJSON) {
         if (broccoliJSON.type == 'queryString') {
             var nrOfInstances = 10000;
+            wl.broccoliClient.lastQueryString = broccoliJSON.queryString;
+            wl.broccoliClient.broccoliHost = broccoliJSON.broccoliHost;
+            wl.broccoliClient.broccoliPort = broccoliJSON.broccoliPort;
             wl.client.getBroccoliInstances(broccoliJSON.queryString,
             broccoliJSON.broccoliHost, broccoliJSON.broccoliPort,
             nrOfInstances, receiveBroccoliInstances);
         }
     };
+
     var receiveBroccoliInstances = function(broccoliResultJSON) {
         var xmlResultString = broccoliResultJSON.responseBody;
         var xmlResult = $($.parseXML(xmlResultString));
         var broccoliInstances = xmlResult.find("i");
         wl.lists.addBroccoliInstances(broccoliInstances);
+    };
+
+    this.getHitGroupForElement = function (instanceElement, callback) {
+        var instanceHref = $(instanceElement).attr('href');
+        var hitGroupNr = wl.lists.getHitGroupNr(instanceHref);
+        // we will extract the hit group of the result before using the given
+        // callback (given callback will get hitgroup xml as input);
+        var callbackForHitgroup = receiveHitgroup.bind(this, callback);
+        wl.client.getBroccoliHitGroup(hitGroupNr, 
+            wl.broccoliClient.lastQueryString, wl.broccoliClient.broccoliHost,
+            wl.broccoliClient.broccoliPort, callbackForHitgroup);        
+    };
+    var receiveHitgroup = function(callbackForHitGroup, broccoliHitgroupJSON) {
+        var xmlResultString = broccoliHitgroupJSON.responseBody;
+        var xmlResult = $($.parseXML(xmlResultString));
+        var hitGroup = xmlResult.find("group").first(); // there shold be only one anyways :)
+        callbackForHitGroup(hitGroup);        
     };
 };
 
--- logic/lists.js
+++ logic/lists.js
@@ -3,34 +3,60 @@ var wl = wikiLists;
 wl.lists = new function(){
     this.lastListElements = [];
     this.hrefToBroccoliHitNumber = {};
-    this.addBroccoliInstances = function (broccoliInstances) {
+
+    var BroccoliInstance = function(broccoliInstanceXML, hitGroupIndex) {
+        var xmlInstanceText = $(broccoliInstanceXML).text()
+        var wikiTitleLink = 
+            wl.lists.extractHrefOfBroccoliInstanceText(xmlInstanceText);
+        this.wikiHref = '/wiki/' + wikiTitleLink;
+        this.title = wikiTitleLink.replace(/_/g, ' ');
+        this.hitGroupIndex = hitGroupIndex;
+    };
+    this.getHitGroupNr = function(wikiHref) {
+        return wl.lists.hrefToBroccoliHitNumber[wikiHref];
+    };
+    this.addBroccoliInstances = function (broccoliInstancesXML) {
         var newBroccoliListElements = [];
         var broccoliListElementsMatchedByWikiList = [];
-        for (var i = 0; i < broccoliInstances.length; i++) {
-            var broccoliInstance = broccoliInstances[i];
+        for (var i = 0; i < broccoliInstancesXML.length; i++) {
+            var instanceXML = broccoliInstancesXML[i]
+            var broccoliInstance = new BroccoliInstance(instanceXML, i);
             addBroccoliInstance(broccoliInstance, newBroccoliListElements,
                 broccoliListElementsMatchedByWikiList);
         }
         $(newBroccoliListElements).each(function() {
-            $('.mw-content-ltr').prepend(this); });
+            $('.mw-content-ltr').prepend(' ');
+            $('.mw-content-ltr').prepend(this); 
+            $(this).mouseover(function() {
+                wl.broccoliClient.getHitGroupForElement(this, 
+                    function(hitgroup) {
+                        console.log("inside, response:\n" + $(hitgroup).text());
+                        });
+                });
+            });
+        $('.mw-content-ltr').prepend('<h2>New Elements</h2>')
         wl.UIMenu.loadBroccoliLists(broccoliListElementsMatchedByWikiList,
             newBroccoliListElements);
     };
     var addBroccoliInstance = function(broccoliInstance,
         newBroccoliListElements, broccoliListElementsMatchedByWikiList) {
-        var wikiTitleLink = 
-            wl.lists.extractHrefOfBroccoliInstanceText($(broccoliInstance).text());
-        var wikiHref = '/wiki/' + wikiTitleLink;
+        var wikiHref = broccoliInstance.wikiHref;
         var isNewElement = !wikiListElementsContainHref(wikiHref);
         var linkElement = $(document.createElement('a')).
                 attr('href', wikiHref).get();        
-        $(linkElement).html(wikiTitleLink.replace(/_/g, ' '));
+        $(linkElement).html(broccoliInstance.title);
         if (isNewElement) {
             newBroccoliListElements.push(linkElement);
         } else {
             broccoliListElementsMatchedByWikiList.push(linkElement);
         }
+        rememberHitGroupIndexInMap(wikiHref, broccoliInstance.hitGroupIndex);
     };
+
+    var rememberHitGroupIndexInMap = function(wikiHrefOfInstance, hitGroupIndex) {
+        wl.lists.hrefToBroccoliHitNumber[wikiHrefOfInstance] = hitGroupIndex;
+    };
+
     this.extractHrefOfBroccoliInstanceText = function(instanceText) {
         // format  of instancetext:
         // :e:<entityname>:<wikipedia-href-link>
@@ -39,9 +65,14 @@ wl.lists = new function(){
         // assumption: there should be no ':' inside the wikipedia link!
         return instanceText.slice(instanceText.lastIndexOf(':') + 1);
     };
+
     var wikiListElementsContainHref = function (wikiHref) {
         return wl.lists.lastListElements.is(function() {
-            return $(this).attr('href') == wikiHref;});
+            return elementHasHref(this, wikiHref);});
     };
-};
 
+    var elementHasHref = function (actualElement, expectedHref) {
+        return $(actualElement).attr('href') == expectedHref;
+    };
+    
+};
