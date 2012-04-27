9d0b0145659bffac732bd69060f51dc88a89b393
ide user ide_robintibor <robintibor@gmail.com>
2012-04-27 14:21:37 +0000

    added both robin and simon cloud9 to bookmarletcode and corrected wrong spelling (init.js)

--- bookmarkletcode.js
+++ bookmarkletcode.js
@@ -1,4 +1,7 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://c9.io/ikset/wikilists/workspace/initt.js';})();
+//cloud9 simon:
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://c9.io/ikset/wikilists/workspace/init.js';})();
+// cloud9 robin:
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://c9.io/robintibor/wikilists/workspace/init.js';})();
 // for github:
 javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.github.com/robintibor/WikiLists/master/github-init.js';})();
 // for stromboli:
